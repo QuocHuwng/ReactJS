f83c483667adde8e099f85a7fb21fe7e9134c147
QuocHuwng <nguyenhung2952006@gmail.com>
2025-08-04 08:07:12 +0700

    Add files via upload

--- PTIT_CNTT4_IT104_Session1/bai1.js
+++ PTIT_CNTT4_IT104_Session1/bai1.js
@@ -2,3 +2,5 @@ for (let i = 0; i < 5;i++){
     console.log("Gia tri i cua vong lap", i);
 }
 console.log("Gia tri i ngoai vong lap", i);
+// lỗi: i không được định nghĩa ở phạm vi này vì let chỉ có phạm vi trong vòng lặp 
+// do đó biến i không thể truy cập dược bên ngoài vòng lặp for
